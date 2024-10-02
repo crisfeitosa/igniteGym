51d38c2a40bdcbf594f4d7c6ec69d087138ea1ae
Cristiano Feitosa <cris.immobilis@gmail.com>
2024-10-01 21:29:09 -0300

    Add splash

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -16,7 +16,7 @@ export function Routes() {
   }
 
   return (
-    <Box flex={1} bg="gray.700">
+    <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
       {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
